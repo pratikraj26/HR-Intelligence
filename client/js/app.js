72dd6a2ac05fc106f7cad4aab31084c9a638833b
Shaivi Vashistha <shaivi.vashistha@gmail.com>
2017-07-04 05:12:10 +0530

    remove eval controller

--- client/js/app.js
+++ client/js/app.js
@@ -18,10 +18,6 @@ hri.config(function ($routeProvider, $compileProvider) {
     templateUrl: 'views/login.html',
     controller: 'loginController'
   }).
-  when('/evaluate/:token', {
-    templateUrl: 'views/evaluate.html',
-    controller: 'evaluateController'
-  }).
   when('/addEmployee', {
     templateUrl: 'views/addEmp.html',
     controller: 'landingController'
